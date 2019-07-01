475c1d51768b900194752c01f35c9293a9dfc71f
mj2913 <51012152+mj2913@users.noreply.github.com>
2019-07-01 00:20:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 function myFunction() {
-  alert(document.getElementById("innerHTML_sign").innerHTML);
+  /*declaring popup variable*/
+  var popup = document.getElementById("happyPopup");
+  popup.classList.toggle("show");
+}
+
+/*storing multiple emotion values in one variable using arrays*/
+var emotionsArray = [
+  "happy",
+  "sad",
+  "surprised",
+  "anxious",
+  "fearful",
+  "confident",
+  "tired",
+  "joyful",
+  "discouraged",
+  "pitiful",
+  "impatient",
+  "cautious",
+  "unloved",
+  "hopeless",
+  "confused",
+  "stressed"
+];
+
+/*establishing element variables*/
+var inputElement = document.getElementById("suggestions");
+var errorElement = document.getElementById("error");
+var wordsElement = document.getElementById("words");
+
+/*creating function for adding words and sending error message if entry is blank*/
+function addWord() {
+  errorElement.innerHTML = "";
+  var words = inputElement.value;
+  if (words.trim() === "") errorElement.innerHTML = "Empty input";
+  else emotionsArray.push(words);
+  inputElement.value = "";
+}
+
+/*adding new words to array*/
+function submit() {
+  words.innerHTML = emotionsArray.join(" - ");
 }
